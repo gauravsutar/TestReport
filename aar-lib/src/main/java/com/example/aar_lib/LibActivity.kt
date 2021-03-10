fcfd1499c76e572a869a86014bc7370119096803
Gaurav Sutar <Gaurav.X.Sutar.-ND@disney.com>
2021-03-10 15:32:11 +0530

    - minor changes

--- aar-lib/src/main/java/com/example/aar_lib/LibActivity.kt
+++ aar-lib/src/main/java/com/example/aar_lib/LibActivity.kt
@@ -2,10 +2,16 @@ package com.example.aar_lib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aar_lib.databinding.ActivityLibBinding
 
 class LibActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLibBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lib)
+        binding = ActivityLibBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
